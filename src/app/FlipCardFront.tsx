321343d6b61b2b7374985a6f70d86924e71f2718
HildLuger <88519194+HildLuger@users.noreply.github.com>
2024-01-11 01:10:22 -0300

    first commit

---
+++ src/app/FlipCardFront.tsx
@@ -0,0 +1,10 @@
+// src/components/FlipCardFront.tsx
+import React from 'react';
+
+const FlipCardFront = () => (
+  <div className="front">
+    <img src="https://picsum.photos/id/411/300/200" alt="Front" />
+  </div>
+);
+
+export default FlipCardFront;
