0dde8c4cfbfcb65a87eb19fd680f39f2481a0530
HildLuger <88519194+HildLuger@users.noreply.github.com>
2024-01-11 02:42:07 -0300

    Update FlipCardFront.tsx
    erase flipcard

--- src/app/FlipCardFront.tsx
+++ src/app/FlipCardFront.tsx
@@ -1,10 +1,1 @@
-// src/components/FlipCardFront.tsx
-import React from 'react';
 
-const FlipCardFront = () => (
-  <div className="front">
-    <img src="https://picsum.photos/id/411/300/200" alt="Front" />
-  </div>
-);
-
-export default FlipCardFront;
